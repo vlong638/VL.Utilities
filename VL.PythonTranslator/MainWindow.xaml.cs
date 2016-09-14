6355d8556d79b1dea9ed569a8792eebcdad67a9e
vlong638 <vlong638@163.com>
2016-09-14 18:11:06 +0800

    0914支持Python处理

--- VL.PythonTranslator/MainWindow.xaml.cs
+++ VL.PythonTranslator/MainWindow.xaml.cs
@@ -26,42 +26,153 @@ namespace VL.PythonTranslator
             InitializeComponent();
         }
 
+        enum MultilineType
+        {
+            None,
+            Backslash,
+            TripleDoubleQuote,
+            TripleSingleQuote,
+        }
         private void button_Click(object sender, RoutedEventArgs e)
         {
             StringReader reader;
             StringBuilder result = new StringBuilder();
-            var textLine = "";
-            //Reference
+            var currentText = "";
+            ////Reference
             if (cb_addReference.IsChecked.HasValue && cb_addReference.IsChecked.Value)
             {
                 result.AppendLine("import PrintHelper");
             }
-            //Title
-            result.AppendLine("PrintHelper.PrintTitle('" + tb_Title.Text + "')");
-            //Definitions
+            ////Title
+            if (!string.IsNullOrEmpty(tb_Title.Text))
+            {
+                result.AppendLine("PrintHelper.PrintTitle('" + tb_Title.Text + "')");
+            }
+            ////Definitions
+            //Oriented
             var definitions = tb_definitions.Text;
-            if (cb_keepOrient.IsChecked.HasValue&& cb_keepOrient.IsChecked.Value)
+            if (cb_keepOrient.IsChecked.HasValue && cb_keepOrient.IsChecked.Value)
             {
                 reader = new StringReader(definitions);
-                while (!string.IsNullOrEmpty(textLine = reader.ReadLine()))
+                while (!string.IsNullOrEmpty(currentText = reader.ReadLine()))
                 {
-                    result.AppendLine(textLine);
+                    result.AppendLine(currentText);
                 }
             }
+            //Print
             reader = new StringReader(definitions);
-            while (!string.IsNullOrEmpty(textLine = reader.ReadLine()))
+            while (!string.IsNullOrEmpty(currentText = reader.ReadLine()))
             {
-                result.AppendLine(tb_prefixForDefinitions.Text + textLine + tb_suffixForDefinitions.Text);
+                if (currentText.Contains("  "))
+                {
+                    var texts = currentText.Split(new string[1] { "  " }, StringSplitOptions.RemoveEmptyEntries);
+                    result.AppendLine("PrintHelper.PrintSampleWithDescription('" + GetEscapeSequencedText(texts[0]) + "','" + GetEscapeSequencedText(texts[1]) + "')");
+                }
+                else
+                {
+                    result.AppendLine(tb_prefixForDefinitions.Text + GetEscapeSequencedText(currentText) + tb_suffixForDefinitions.Text);
+                }
+                //AppendPrintText(tb_prefixForDefinitions.Text, tb_suffixForDefinitions.Text, result, currentText);
             }
-            //Samples
+            ////Samples
             var samples = tb_samples.Text;
             reader = new StringReader(samples);
-            while (!string.IsNullOrEmpty(textLine = reader.ReadLine()))
+            currentText = reader.ReadLine();
+            string previousText = "";
+            List<string> previousTexts = new List<string>();
+            MultilineType currentMultilineType = MultilineType.None;
+            if (!string.IsNullOrEmpty(currentText))
             {
-                result.AppendLine("PrintHelper.PrintSubtitle('" + textLine + "')");
-                result.AppendLine(textLine);
+                do
+                {
+                    if (currentText.StartsWith(" ") || currentMultilineType != MultilineType.None)
+                    {
+                        switch (currentMultilineType)
+                        {
+                            case MultilineType.None:
+                                break;
+                            case MultilineType.Backslash:
+                                if (!currentText.EndsWith("\\"))
+                                {
+                                    currentMultilineType = MultilineType.None;
+                                }
+                                break;
+                            case MultilineType.TripleDoubleQuote:
+                                if (currentText.Contains("\"\"\""))
+                                {
+                                    currentMultilineType = MultilineType.None;
+                                }
+                                break;
+                            case MultilineType.TripleSingleQuote:
+                                if (currentText.Contains("\'\'\'"))
+                                {
+                                    currentMultilineType = MultilineType.None;
+                                }
+                                break;
+                            default:
+                                throw new NotImplementedException("not implemented with type " + currentMultilineType.ToString());
+                        }
+                        previousTexts.Add(currentText);
+                    }
+                    else
+                    {
+                        PrintPreviousTexts(result, previousTexts);
+                        previousTexts = new List<string>() { currentText };
+                        if (currentText.EndsWith("\\"))
+                        {
+                            currentMultilineType = MultilineType.Backslash;
+                        }
+                        else if (currentText.Contains("\"\"\""))
+                        {
+                            currentMultilineType = MultilineType.TripleDoubleQuote;
+                        }
+                        else if (currentText.Contains("\'\'\'"))
+                        {
+                            currentMultilineType = MultilineType.TripleSingleQuote;
+                        }
+                    }
+                    previousText = currentText;
+                }
+                while (!string.IsNullOrEmpty(currentText = reader.ReadLine()));
+                if (previousTexts.Count > 0)
+                {
+                    PrintPreviousTexts(result, previousTexts);
+                }
             }
             tb_output.Text = result.ToString();
+        }
+
+        private static void AppendPrintText(string prefix,string suffix,StringBuilder result, string currentText)
+        {
+            if (currentText.Contains("  "))
+            {
+                var texts = currentText.Split(new string[1] { "  " }, StringSplitOptions.RemoveEmptyEntries);
+                result.AppendLine("PrintHelper.PrintSampleWithDescription('" + GetEscapeSequencedText(texts[0]) + "','" + GetEscapeSequencedText(texts[1]) + "')");
+            }
+            else
+            {
+                result.AppendLine(prefix+GetEscapeSequencedText(currentText) + suffix);
+            }
+        }
+
+        private static void PrintPreviousTexts(StringBuilder result, List<string> previousTexts)
+        {
+            foreach (var previousText in previousTexts)
+            {
+                result.AppendLine("PrintHelper.PrintCode('" + GetEscapeSequencedText(previousText) + "')");
+            }
+            foreach (var previousText in previousTexts)
+            {
+                result.AppendLine(previousText);
+            }
+        }
+
+        private static string GetEscapeSequencedText(string previousText)
+        {
+            previousText = previousText.Replace("\\", "\\\\");
+            previousText = previousText.Replace("'", "\\'");
+            previousText = previousText.Replace("\"", "\\\"");
+            return previousText;
         }
     }
 }
